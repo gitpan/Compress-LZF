73758a8c514694b1d75dad1de2c29d40aa0053df
Marc Lehmann <schmorp@schmorp.de>
2005-03-08 20:08:24 +0000

    Import of MLEHMANN/Compress-LZF-1.5 from CPAN.
    gitpan-cpan-distribution: Compress-LZF
    gitpan-cpan-version:      1.5
    gitpan-cpan-path:         MLEHMANN/Compress-LZF-1.5.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- LZF.pm
+++ LZF.pm
@@ -111,7 +111,7 @@ package Compress::LZF;
 require Exporter;
 require DynaLoader;
 
-$VERSION = '1.401';
+$VERSION = '1.5';
 @ISA = qw/Exporter DynaLoader/;
 %EXPORT_TAGS = (
       freeze   => [qw(sfreeze sfreeze_cr sfreeze_c sthaw)],
